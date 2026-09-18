cc3d609d86be39514caf9dfa88f6673b34440558
Kabir99-login <kabirpratapsingh99@gmail.com>
2026-09-18 12:40:15 +0530

    Corrected Home Page

--- Vtiger/src/main/java/object_repo/HomePage.java
+++ Vtiger/src/main/java/object_repo/HomePage.java
@@ -1,10 +1,18 @@
 package object_repo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
 
   public HomePage(WebDriver driver){
     PageFactory.initElements(driver,this);
 
+  }
+ 
+  
+  @FindBy(xpath="//a[.='Contacts']")
+  private WebElement contact;
 }
-  @FindBy(id="username")
-  private WebElement username
