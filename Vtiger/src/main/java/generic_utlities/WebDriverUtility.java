d97dae0e54af86d05d16073d6c6be3b17fed2043
Kabir99-login <kabirpratapsingh99@gmail.com>
2026-09-18 16:43:13 +0530

    Added code in WebDriver Utility

--- Vtiger/src/main/java/generic_utlities/WebDriverUtility.java
+++ Vtiger/src/main/java/generic_utlities/WebDriverUtility.java
@@ -1,5 +1,14 @@
 package generic_utlities;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WebDriverUtility {
+	WebDriver driver;
+	public WebDriver launchBrowser(String browser) {
+		if(browser.equalsIgnoreCase("chrome"))
+			driver= new ChromeDriver();
+		return driver;
+	}
 
 }
