c4dd3030181af49dc231d0262cb38cdc421970da
Kabir99-login <kabirpratapsingh99@gmail.com>
2026-09-18 12:32:41 +0530

    Add constructor to HomePage for PageFactory initialization
    Added constructor to initialize PageFactory elements.

--- Vtiger/src/main/java/object_repo/HomePage.java
+++ Vtiger/src/main/java/object_repo/HomePage.java
@@ -2,4 +2,9 @@ package object_repo;
 
 public class HomePage {
 
+  public HomePage(WebDriver driver){
+    PageFactory.initElements(driver,this);
+
 }
+  @FindBy(id="username")
+  private WebElement username
